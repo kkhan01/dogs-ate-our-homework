603ea6755b2b332ff2e6e72232dbc9dc6f2a9535
kkhan01 <kkhan@stuy.edu>
2018-01-26 14:51:59 +0000

    added headers

--- sdl_tests/sdlUbuntu/tetris.c
+++ sdl_tests/sdlUbuntu/tetris.c
@@ -10,12 +10,19 @@ unsigned int tetronimos[7][4] = {
     {0b0110110000000000, 0b0100011000100000, 0b0000011011000000, 0b1000110001000000}  // S    
 };
 
-// Returns absolute position of the block
+/*======== unsigned tet_location ==========
+Inputs:  int x
+         int y
+Returns: Returns absolute position of the block
+=======================================*/
 unsigned int tet_location(int x, int y) {
     return 0b1000000000000000 >> (x + y * 4);
 }
 
-// Checks if the block is overlapping anything. True if not overlapping.
+/*======== unsigned is_tet_legal ==========
+Inputs:  struct tet_block tet
+Returns: Checks if the block is overlapping anything. True if not overlapping.
+=======================================*/
 boolean is_tet_legal(struct tet_block tet) {
     for (int i = 0; i < 4; i++) {
         for (int j = 0; j < 4; j++) {
@@ -30,6 +37,10 @@ boolean is_tet_legal(struct tet_block tet) {
     return 1;
 }
 
+/*======== unsigned tet_wall_kick ==========
+Inputs:  struct tet_block tet
+Returns: success or failure based on wher it was able to bounce
+=======================================*/
 boolean tet_wall_kick(struct tet_block *tet) {
     tet->x -= 1; // Try to move it left
     if (is_tet_legal(*tet))
